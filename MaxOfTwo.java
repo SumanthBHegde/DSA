c09a506847d4a97eaf9928eb02728517ff545719
Sumanth Hegde <sumanthhegde002@gmail.com>
2025-12-23 23:39:19 +0530

    Implement maxProduct method to find max product

---
+++ MaxOfTwo.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        if (nums.length < 2 || nums.length > 501) return 0;
+        int max1 = 0;
+        int max2 = 0;
+        if (nums[0] > nums[1]){
+            max1 = nums[0];
+            max2 = nums[1];
+        }
+        else{
+            max1 = nums[1];
+            max2 = nums[0];
+        }
+        for (int i = 2; i < nums.length; i++){
+            if (max1 <= nums[i]){
+                max2 = max1;
+                max1 = nums[i];
+            }
+            else if (max2 <= nums[i]){
+                max2 = nums[i];
+            }
+        }
+        return (max1 - 1)*(max2 - 1);
+    }
+}
